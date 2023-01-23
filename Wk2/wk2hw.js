87aabfe2c706468c2cee5b9667a5c6d3b6afeb18
Doc Carson <kvcarsonjr@gmail.com>
2023-01-22 20:06:03 -0500

    Wk2

---
+++ Wk2/wk2hw.js
@@ -0,0 +1,11 @@
+const pizzaPlace = "Docs Pizza";
+let numberOfToppings = 10;
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+console.log(`This is ${pizzaPlace}, We have ${numberOfToppings} different toppings.`);
+ if (numberOfToppings < 10) {
+   console.log('Quality, not quantity');
+ } else {
+   console.log('A whole lot of pizza')
+ }
