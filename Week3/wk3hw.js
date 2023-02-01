393649425d5b961ee9cff86061f6e92772f524f8
Doc Carson <kvcarsonjr@gmail.com>
2023-02-01 09:38:41 -0500

    week 3 homework

--- Week3/wk3hw.js
+++ Week3/wk3hw.js
@@ -2,17 +2,17 @@
 let pizzaToppings = ["sausage", "chicken", "peppers", "mushrooms"];
 
 //2
-let greetCustomer() {
+function greetCustomer() {
   console.log("Welcome to Pizza House our toppings are ${pizzaToppings}")
 }
 
 //3
-let getPizzaOrder(size crust ...toppings) {
+function getPizzaOrder(size crust ...toppings) {
   console.log(`One ${size} ${crust} crust pizza with ${toppings.join(", ")} coming up!`);)
 };
 
 //4
-let preparePizza(size, crust, toppings) {
+function preparePizza(size, crust, toppings) {
   console.log("Cooking pizza");
   return {
   size,
